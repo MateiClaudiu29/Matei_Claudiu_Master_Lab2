a783d43a322900cbd3c601217efb115fe0e4b3ea
CLAUDIU-GABRIEL MATEI <claudiu.gabriel.matei@stud.ubbcluj.ro>
2022-11-06 22:15:26 +0200

    Before tasks

---
+++ Matei_Claudiu_Lab2/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matei_Claudiu_Lab2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Auhor { get; set; }
+        public decimal Price { get; set; }  
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
